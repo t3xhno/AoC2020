c69eec73a70b3fd4fcc6786e4ee0aa56d6f268ad
texhnolainx <texhnolainx@gmail.com>
2020-12-04 12:11:34 +0100

    Day 1 (JavaScript)

---
+++ Problem_1/solution_1.js
@@ -0,0 +1,25 @@
+const readline = require('readline');
+const fs = require('fs');
+
+let dataset = [];
+
+const main = async () => {
+    const readInterface = readline.createInterface({
+        input: fs.createReadStream('./dataset.txt', 'utf-8')
+    });
+
+    for await (const line of readInterface) {
+        dataset.push(Number(line));
+    }
+
+    for (let i = 0; i < dataset.length - 1; i++) {
+        for (let j = i + 1; j < dataset.length; j++) {
+            if (dataset[i] + dataset[j] === 2020) {
+                console.log(`${dataset[i]} + ${dataset[j]} (sum = ${dataset[i] + dataset[j]}) = ${dataset[i] * dataset[j]}`);
+                return dataset[i] * dataset[j];
+            }
+        }
+    }
+};
+
+main();
---
+++ Problem_1/solution_2.js
@@ -0,0 +1,27 @@
+const readline = require('readline');
+const fs = require('fs');
+
+let dataset = [];
+
+const main = async () => {
+    const readInterface = readline.createInterface({
+        input: fs.createReadStream('./dataset.txt', 'utf-8')
+    });
+
+    for await (const line of readInterface) {
+        dataset.push(Number(line));
+    }
+
+    for (let i = 0; i < dataset.length - 2; i++) {
+        for (let j = i + 1; j < dataset.length - 1; j++) {
+            for (let k = j + 1; k < dataset.length; k++) {
+                if (dataset[i] + dataset[j] + dataset[k] === 2020) {
+                    console.log(`${dataset[i]} + ${dataset[j]} + ${dataset[k]} (sum = ${dataset[i] + dataset[j] + dataset[k]}) = ${dataset[i] * dataset[j] * dataset[k]}`);
+                    return dataset[i] * dataset[j];
+                }
+            }
+        }
+    }
+};
+
+main();
