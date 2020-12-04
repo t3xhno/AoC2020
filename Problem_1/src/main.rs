86f283453aa18661a248ba21406dc29a766ee5df
texhnolainx <t3xhnolainx@gmail.com>
2020-12-04 23:37:23 +0100

    Changes

--- Problem_1/src/main.rs
+++ Problem_1/src/main.rs
@@ -3,10 +3,7 @@ use std::fs;
 const FILE_NAME: &str = "dataset.txt";
 
 fn main () {
-    let dataset = match fs::read_to_string(FILE_NAME) {
-        Ok(value) => value,
-        Err(e) => e.to_string()
-    };
+    let dataset = fs::read_to_string(FILE_NAME).expect("Something went wrong!");
     let mut salaries: Vec<u16> = vec![];
 
     for word in dataset.split_whitespace() {
@@ -16,10 +13,8 @@ fn main () {
     for i in 0..salaries.len() - 1 {
         for j in i + 1..salaries.len() {
             match salaries[i] + salaries[j] {
-                2020 => {
-                    println!("{}", salaries[i] * salaries[j]);
-                },
-                _ => (),
+                2020 => println!("{}", salaries[i] * salaries[j]),
+                _ => ()
             }
         }
     }
