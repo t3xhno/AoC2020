06eb307d4ab3306014fd6ff89caa1e854673bcab
texhnolainx <t3xhnolainx@gmail.com>
2020-12-05 15:33:53 +0100

    Removed cached files that are in .gitignore now

--- Problem_1/target/rls/debug/deps/problem_1-1668d13b471a7f07.d
+++
@@ -1,7 +0,0 @@
-/home/tex/Projects/AoC2020/Problem_1/target/rls/debug/deps/problem_1-1668d13b471a7f07.rmeta: src/main.rs src/solution_1.rs src/solution_2.rs
-
-/home/tex/Projects/AoC2020/Problem_1/target/rls/debug/deps/problem_1-1668d13b471a7f07.d: src/main.rs src/solution_1.rs src/solution_2.rs
-
-src/main.rs:
-src/solution_1.rs:
-src/solution_2.rs:
