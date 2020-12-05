11bbc069725d480970e3a01a475b6dd0ef1f1525
texhnolainx <t3xhnolainx@gmail.com>
2020-12-05 19:47:17 +0100

    Moved KEYS to function

--- Problem_4/src/solution_1.rs
+++ Problem_4/src/solution_1.rs
@@ -1,12 +1,12 @@
-fn check_keys (keys: &Vec<&str>, keys_const: [&str; 7]) -> bool {
-    for (_i, &key) in keys_const.iter().enumerate() {
+fn check_keys (keys: &Vec<&str>) -> bool {
+    const KEYS: [&str; 7] = ["byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"];
+    for (_i, &key) in KEYS.iter().enumerate() {
         if !keys.contains(&key) { return false; }
     }
     true
 }
 
 pub fn run (dataset: &String) {
-    const KEYS: [&str; 7] = ["byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"];
     let passports: Vec<&str> = dataset.split_terminator("\n\n").collect();
     let mut number_of_valid = 0;
     for (_i, &passport) in passports.iter().enumerate() {
@@ -15,7 +15,7 @@ pub fn run (dataset: &String) {
         for (_j, &pair) in pairs.iter().enumerate() {
             keys.push(pair.split_terminator(":").collect::<Vec<&str>>()[0]);
         }
-        if check_keys(&keys, KEYS) { number_of_valid += 1; }
+        if check_keys(&keys) { number_of_valid += 1; }
     }
     println!("{}", number_of_valid);
 }
