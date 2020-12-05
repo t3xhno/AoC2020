b908f836a662ec6dc828d26b0ae62f7ec301da8b
texhnolainx <t3xhnolainx@gmail.com>
2020-12-05 15:28:14 +0100

    Added

--- Problem_2/src/main.rs
+++ Problem_2/src/main.rs
@@ -9,9 +9,10 @@ fn main () {
         println!("Please enter either problem_1 or problem_2.");
     } else if args.len() == 2 {
         match args[1].as_str() {
-            "problem_1" => solution_1::run(),
+            // "problem_1" => solution_1::run(),
             "problem_2" => solution_2::run(),
             _ => println!("Invalid argument!")
         }
     }
+    println!("Hello.");
 }
