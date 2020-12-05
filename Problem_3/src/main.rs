526e088cdb49c1771d4e2e29d86f87a4fa6c081f
texhnolainx <t3xhnolainx@gmail.com>
2020-12-05 18:35:59 +0100

    Day_3 Solution_2 (Rust)

---
+++ Problem_3/src/main.rs
@@ -0,0 +1,21 @@
+use std::fs;
+use std::env;
+
+mod solution_1;
+mod solution_2;
+
+const FILE_NAME: &str = "dataset.txt";
+
+fn main () {
+    let dataset = fs::read_to_string(FILE_NAME).expect("Something went wrong...");
+    let args: Vec<String> = env::args().collect();
+    if args.len() != 2 {
+        println!("Please enter either problem_1 or problem_2.");
+    } else if args.len() == 2 {
+        match args[1].as_str() {
+            "problem_1" => solution_1::run(dataset),
+            "problem_2" => solution_2::run(dataset),
+            _ => println!("Invalid argument!")
+        }
+    }
+}
---
+++ Problem_3/src/solution_1.rs
@@ -0,0 +1,13 @@
+pub fn run (dataset: String) {
+    let matrix: Vec<&str> = dataset.split_terminator("\n").collect();
+    let mut count_trees = 0;
+    let mut pos_x = 0;
+
+    for i in 0..matrix.len() {
+        if matrix[i].chars().nth(pos_x % matrix[0].len()).unwrap() == "#".chars().nth(0).unwrap() {
+            count_trees += 1;
+        }
+        pos_x += 3;
+    }
+    println!("{}", count_trees);
+}
